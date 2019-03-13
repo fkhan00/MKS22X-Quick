83e5d684bda94803008117ae7240872d9718d26a
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-12 21:04:38 -0400

    quickSort works most of the time on the same ary

--- Quick.java
+++ Quick.java
@@ -4,9 +4,8 @@ public class Quick{
     if(start == end){
       return start;}
     Random randgen = new Random();
-    int index = Math.abs(randgen.nextInt()) % (end - start);
+    int index = Math.abs(randgen.nextInt() % (end - start)) + start;
     // randomly chosen element to be pivot
-    index = median(index, start, end);
     int hold = ary[start];
     ary[start] = ary[index];
     ary[index] = hold;
@@ -17,26 +16,18 @@ public class Quick{
     for(int i = start + 1; i < end; i++){
       //if element is less than pivot
       // swap it with pivot and update index
-      if(ary[i] < ary[index]){
+      if(ary[i] <= ary[index]){
         hold = ary[i];
         ary[i] = ary[index];
         ary[index] = hold;
-        index = i;}
-      else if(ary[i] == ary[index]){
-        if(randgen.nextInt() % 2 == 0){
-          hold = ary[i];
-          ary[i] = ary[index];
-          ary[index] = hold;
-          index = i;}
-        else{}
-      }
+        index ++;}
       else{
       //if element is greater than pivot
       // move from back of list until you reach element smaller than pivot
       // swap that element with the element greater than pivot and then pivot with the smaller element
       // once point equals index, the array has been sorted and now return index
         while(ary[point] > ary[index]){
-          if(point <= index + 1){
+          if(point == index + 1){
             return index;}
           point --;}
         hold = ary[point];
@@ -44,12 +35,6 @@ public class Quick{
         ary[i] = hold;
         i--;}}
     return index;}
-  public static int median(int x, int y, int z){
-    if(x >= y && y >= z){
-      return y;}
-    if(x <= y && z >= x){
-      return x;}
-    return z;}
   public static int quickSelect(int[] ary, int k){
     int start = 0;
     int end = ary.length - 1;
@@ -65,9 +50,16 @@ public class Quick{
       popped = partition(ary, 0, end);}
       else{
         return ary[popped];}}}
+    public static void quickSort(int[] ary){
+      quickSortH(ary, 0, ary.length - 1);}
+    public static void quickSortH(int[] ary, int start, int end){
+        if(end > start){
+        int popped = partition(ary, start, end );
+        quickSortH(ary, start, popped - 1);
+        quickSortH(ary, popped + 1, end);}}
     public static String toString(int[] ary){
       String output = "";
       for(int i = 0; i < ary.length; i++){
         output += ary[i] + " ";}
       return output;
-}}
+    }}
