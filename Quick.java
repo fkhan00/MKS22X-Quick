fbf4b0e1e6ea1b66ac233b06a60f2e9e7c44c7c2
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-14 18:40:17 -0400

    my quicksort can only handle a 250,000 random elements in an array of size 10 before overflowing

--- Quick.java
+++ Quick.java
@@ -4,43 +4,33 @@ public class Quick{
       if(start == end){
         return start;}
       Random randgen = new Random();
-      int index = Math.abs(randgen.nextInt()) % (end - start) + start;
+      int index = median(Math.abs(randgen.nextInt()) % (end - start) + start, Math.abs(randgen.nextInt()) % (end - start) + start, Math.abs(randgen.nextInt()) % (end - start) + start);
       // randomly chosen element to be pivot
-      index = median(index, start, end);
       int hold = ary[start];
       ary[start] = ary[index];
       ary[index] = hold;
       index = start;
       //swap pivot with start index
-      int point = end;
       // go through sublist
       for(int i = start + 1; i <= end; i++){
         //if element is less than pivot
         // swap it with pivot and update index
-        if(ary[i] < ary[index]){
+        if(ary[i] < ary[index] || (ary[i] == ary[index] && Math.abs(randgen.nextInt() % 2) == 1)){
           hold = ary[i];
           ary[i] = ary[index];
           ary[index] = hold;
           index = i;}
-        else if(ary[i] == ary[index]){
-          if(randgen.nextInt() % 2 == 0){
-            hold = ary[i];
-            ary[i] = ary[index];
-            ary[index] = hold;
-            index = i;}
-          else{}
-        }
         else{
         //if element is greater than pivot
         // move from back of list until you reach element smaller than pivot
         // swap that element with the element greater than pivot and then pivot with the smaller element
         // once point equals index, the array has been sorted and now return index
-          while(ary[point] > ary[index]){
-            if(point <= index + 1){
+          while(ary[end] >= ary[index]){
+            if(end <= index + 1){
               return index;}
-            point --;}
-          hold = ary[point];
-          ary[point] = ary[i];
+            end --;}
+          hold = ary[end];
+          ary[end] = ary[i];
           ary[i] = hold;
           i--;}}
   return index;}
@@ -58,24 +48,59 @@ public class Quick{
     while(true){
       popped = partition(ary, start, end);
         if(popped < k){
-        start = popped + 1;
-      }
+        start = popped + 1;}
       else if(popped > k){
-      end = popped - 1;
-    }
+      end = popped - 1;}
       else{
         return ary[popped];}}}
+
     public static void quicksort(int[] ary){
-      quickSortH(ary, 0, ary.length - 1);}
-    public static void quickSortH(int[] ary, int start, int end){
+      quicksortH(ary, 0, ary.length - 1, 0);}
+    public static void quicksortH(int[] ary, int start, int end, int popped){
       if(end > start){
-        int popped = partition(ary, start, end);
-        quickSortH(ary, start, popped - 1);
-        quickSortH(ary, popped + 1, end);}}
+        popped = partition(ary, start, end);
+        quicksortH(ary, start, popped - 1, popped);
+        quicksortH(ary, popped + 1, end, popped);}}
 
     public static String toString(int[] ary){
       String output = "";
       for(int i = 0; i < ary.length; i++){
         output += ary[i] + " ";}
       return output;
-}}
+}
+  public static int partitionDutch(int[] ary, int lo, int hi){
+    Random randgen = new Random();
+    if(hi == lo){
+      return lo;}
+    int index = Math.abs(randgen.nextInt()) % (hi - lo ) + lo;
+    index = median(index, lo, hi);
+    int hold = ary[lo];
+    ary[lo] = ary[index];
+    ary[index] = hold;
+    index = lo;
+    int li = lo;
+    int point = hi;
+    for(int i = lo + 1; i <= hi; i++){
+      while(ary[index] == ary[i]){
+        i ++;
+        index ++;}
+      if(ary[index] > ary[i]){
+        hold = ary[lo];
+        ary[lo] = ary[i];
+        ary[i] = hold;
+        lo ++;}
+      else{
+        while(ary[point] > ary[index]){
+          if(point <= index + 1){
+            return index;}
+          point --;}
+        hold = ary[point];
+        ary[point] = ary[i];
+        ary[i] = hold;
+        i--;}}
+    return index;}
+
+
+
+
+}
