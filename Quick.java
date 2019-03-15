81358f96eed69033479a807c7ec1d6bb5fb575db
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-15 06:06:09 -0400

    couldn't fix StackOverflow at 250,000 array with max value of 10

--- Quick.java
+++ Quick.java
@@ -1,38 +1,41 @@
-import java.util.Random;
+import java.util.*;
 public class Quick{
     public static int partition(int[] ary, int start, int end){
       if(start == end){
         return start;}
       Random randgen = new Random();
-      int index = median(Math.abs(randgen.nextInt()) % (end - start) + start, Math.abs(randgen.nextInt()) % (end - start) + start, Math.abs(randgen.nextInt()) % (end - start) + start);
-      // randomly chosen element to be pivot
+      int keeper = (Math.abs(randgen.nextInt()) % (end - start)) + start;
+      int index =  median(ary[start], ary[end], ary[keeper]);
+      if(index == ary[start]){
+        index = start;}
+      else if (index == ary[end]){
+        index = end;}
+      else{
+        index = keeper;}
       int hold = ary[start];
       ary[start] = ary[index];
       ary[index] = hold;
       index = start;
-      //swap pivot with start index
-      // go through sublist
       for(int i = start + 1; i <= end; i++){
-        //if element is less than pivot
-        // swap it with pivot and update index
-        if(ary[i] < ary[index] || (ary[i] == ary[index] && Math.abs(randgen.nextInt() % 2) == 1)){
+        if(ary[i] < ary[index] || (ary[i] == ary[index] && Math.abs(randgen.nextInt()) % 2 == 0)){
           hold = ary[i];
           ary[i] = ary[index];
           ary[index] = hold;
           index = i;}
         else{
-        //if element is greater than pivot
-        // move from back of list until you reach element smaller than pivot
-        // swap that element with the element greater than pivot and then pivot with the smaller element
-        // once point equals index, the array has been sorted and now return index
           while(ary[end] >= ary[index]){
-            if(end <= index + 1){
+            if(end == index){
               return index;}
             end --;}
           hold = ary[end];
           ary[end] = ary[i];
           ary[i] = hold;
-          i--;}}
+
+          hold = ary[index];
+          ary[index] = ary[i];
+          ary[i] = hold;
+          index =i;
+        }}
   return index;}
     public static int median(int x, int y, int z){
     if(x >= y && y >= z){
@@ -55,52 +58,16 @@ public class Quick{
         return ary[popped];}}}
 
     public static void quicksort(int[] ary){
-      quicksortH(ary, 0, ary.length - 1, 0);}
-    public static void quicksortH(int[] ary, int start, int end, int popped){
+      quicksortH(ary, 0, ary.length - 1);}
+
+    public static void quicksortH(int[] ary, int start, int end){
       if(end > start){
-        popped = partition(ary, start, end);
-        quicksortH(ary, start, popped - 1, popped);
-        quicksortH(ary, popped + 1, end, popped);}}
+        int popped = partition(ary, start, end);
+        quicksortH(ary, start, popped - 1);
+        quicksortH(ary, popped + 1, end);}}
 
     public static String toString(int[] ary){
       String output = "";
       for(int i = 0; i < ary.length; i++){
         output += ary[i] + " ";}
-      return output;
-}
-  public static int partitionDutch(int[] ary, int lo, int hi){
-    Random randgen = new Random();
-    if(hi == lo){
-      return lo;}
-    int index = Math.abs(randgen.nextInt()) % (hi - lo ) + lo;
-    index = median(index, lo, hi);
-    int hold = ary[lo];
-    ary[lo] = ary[index];
-    ary[index] = hold;
-    index = lo;
-    int li = lo;
-    int point = hi;
-    for(int i = lo + 1; i <= hi; i++){
-      while(ary[index] == ary[i]){
-        i ++;
-        index ++;}
-      if(ary[index] > ary[i]){
-        hold = ary[lo];
-        ary[lo] = ary[i];
-        ary[i] = hold;
-        lo ++;}
-      else{
-        while(ary[point] > ary[index]){
-          if(point <= index + 1){
-            return index;}
-          point --;}
-        hold = ary[point];
-        ary[point] = ary[i];
-        ary[i] = hold;
-        i--;}}
-    return index;}
-
-
-
-
-}
+      return output;}}
